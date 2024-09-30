33c97954011d6488790e9a4b9622d2c41e087561
zlnongi <yeossi8@naver.com>
2024-09-30 22:32:47 +0900

    @GetMapping 구현

--- src/main/java/org/example/hello/controller/GetController.java
+++ src/main/java/org/example/hello/controller/GetController.java
@@ -1,8 +1,6 @@
 package org.example.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("/api/v1/get-api")
@@ -11,4 +9,16 @@ public class GetController {
     public String gethello() {
        return "Hello World";
    }
+   // 매개변수 없는 GET 메서드 구현
+   @GetMapping(value = "/name")
+    public String getname() {
+       return "Flature";
+   }
+
+   // 실무 환경에서 매개변수를 받지 않는 메서드 거의 쓰이지 않음
+    // URL 자체에 값을 담아 요청하는 것
+    @GetMapping(value ="/variable1/{variable}")
+    public String getvariable1(@PathVariable String variable) {
+       return variable;
+    }
 }
